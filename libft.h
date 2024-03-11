8f640fc3efb0187e3f0d11bdefe904e5c885a73d
ecaleb97 <valley_rob.0c@icloud.com>
2024-03-11 18:11:03 +0100

    Add .prettierrc and ft_isalpha.c files

---
+++ libft.h
@@ -0,0 +1,15 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+    Return zero if the character tests false and returns
+    non-zero if the character tests true
+*/
+int ft_isalpha(int c);
+
+
+#endif
+
