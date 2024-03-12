a8ee72cdb05d4142014c02044713b30fc9afa0c5
ecaleb97 <valley_rob.0c@icloud.com>
2024-03-12 12:50:52 +0100

    remove string.h and stdio.h

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,18 +1,6 @@
 #include "libft.h"
-#include <string.h>
-#include <stdio.h>
 
 void    ft_bzero(void *s, size_t n)
 {
     ft_memset(s, '\0', n);
-}
-
-int main(void)
-{
-    char s[5] = "Hello";
-    printf("%s\n", s);
-    bzero(s, 1);
-    printf("%s\n", s);
-
-    return (0);
 }
