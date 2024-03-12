19051658ef4b633c67afc7b34725221ff093770a
ecaleb97 <valley_rob.0c@icloud.com>
2024-03-12 12:59:46 +0100

    add ft_memcpy file

---
+++ ft_memcpy.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+    size_t  i;
+
+    if (dst == src || n == 0)
+        return (dst);
+    
+    i = 0;
+    while (i < n)
+    {
+        *((char *)dst + i) = *((char *)src + i);
+        i++;
+    }
+    return (dst);
+}
