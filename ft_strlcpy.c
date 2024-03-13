e4e9ddaa4c124ecd9691f77447f061fc2cad9668
ecaleb97 <valley_rob.0c@icloud.com>
2024-03-13 01:49:07 +0100

    Add ft_strlcpy function to the project

---
+++ ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 01:00:49 by envillan          #+#    #+#             */
+/*   Updated: 2024/03/13 01:48:10 by envillan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *restrict dst,
+			const char *restrict src,
+			size_t dstsize)
+{
+	size_t	i;
+	size_t	src_len;
+
+	src_len = 0;
+	i = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	while (src[i] != '\0' && i + 1 < dstsize)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (dstsize != 0)
+		dst[i] = '\0';
+	return (src_len);
+}
