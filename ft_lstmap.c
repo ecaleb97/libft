6ae553f9187ab6376b1b33f40e4023c28d0cb5ef
ecaleb97 <valley_rob.0c@icloud.com>
2024-03-28 17:12:25 +0100

    Refactor lstmap and split functions

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,11 +6,40 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 12:48:56 by envillan          #+#    #+#             */
-/*   Updated: 2024/03/28 13:26:30 by envillan         ###   ########.fr       */
+/*   Updated: 2024/03/28 16:46:43 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/* static	t_list	*lst_new(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (0);
+	new->content = content;
+	new->next = 0;
+	return (new);
+}
+
+static	void	lst_add_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last = *lst;
+	while (last->next)
+		last = last->next;
+	last->next = new;
+}
 
 static	void	lst_clear(t_list **lst, void (*del)(void *))
 {
@@ -20,7 +49,7 @@ static	void	lst_clear(t_list **lst, void (*del)(void *))
 	(del)((*lst)->content);
 	free(*lst);
 	*lst = 0;
-}
+} */
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -28,18 +57,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_elem;
 	void	*content;
 
-	if (!lst || !f)
+	if (!lst || !f || !del)
 		return (0);
 	new_lst = 0;
 	while (lst)
 	{
-		content = (*f)(lst->content);
-		new_elem = ft_lstnew((*f)(lst->content));
+		content = f(lst->content);
+		new_elem = ft_lstnew(content);
 		if (!new_elem)
 		{
 			del(content);
-			lst_clear(&new_lst, del);
-			return (0);
+			ft_lstclear(&new_lst, del);
+			return (new_lst);
 		}
 		ft_lstadd_back(&new_lst, new_elem);
 		lst = lst->next;
--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 13:09:07 by envillan          #+#    #+#             */
-/*   Updated: 2024/03/28 13:10:57 by envillan         ###   ########.fr       */
+/*   Updated: 2024/03/28 17:10:15 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_count_words(char const *s, char c)
+/* static size_t	ft_count_words(char const *s, char c)
 {
 	size_t	count;
 	size_t	i;
@@ -72,4 +72,90 @@ char	**ft_split(char const *s, char c)
 	}
 	tab[j] = NULL;
 	return (tab);
+} */
+
+static int	numstring(char const *s1, char c)
+{
+	int	comp;
+	int	cles;
+
+	comp = 0;
+	cles = 0;
+	if (*s1 == '\0')
+		return (0);
+	while (*s1 != '\0')
+	{
+		if (*s1 == c)
+			cles = 0;
+		else if (cles == 0)
+		{
+			cles = 1;
+			comp++;
+		}
+		s1++;
+	}
+	return (comp);
+}
+
+static int	numchar(char const *s2, char c, int i)
+{
+	int	lenght;
+
+	lenght = 0;
+	while (s2[i] != c && s2[i] != '\0')
+	{
+		lenght++;
+		i++;
+	}
+	return (lenght);
+}
+
+static char	**ft_free(char const **dst, int j)
+{
+	while (j > 0)
+	{
+		j--;
+		free((void *)dst[j]);
+	}
+	free(dst);
+	return (NULL);
+}
+
+static char	**affect(char const *s, char **dst, char c, int l)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	while (s[i] != '\0' && j < l)
+	{
+		k = 0;
+		while (s[i] == c)
+			i++;
+		dst[j] = (char *)malloc(sizeof(char) * numchar(s, c, i) + 1);
+		if (dst[j] == NULL)
+			return (ft_free((char const **)dst, j));
+		while (s[i] != '\0' && s[i] != c)
+			dst[j][k++] = s[i++];
+		dst[j][k] = '\0';
+		j++;
+	}
+	dst[j] = 0;
+	return (dst);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**dst;
+	int		l;
+
+	if (s == NULL)
+		return (NULL);
+	l = numstring(s, c);
+	dst = (char **)malloc(sizeof(char *) * (l + 1));
+	if (dst == NULL)
+		return (NULL);
+	return (affect(s, dst, c, l));
 }
